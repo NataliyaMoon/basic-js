1cb6ebb9cc6de54d517c6b227c695f5320302453
Moon Natalia <nataliya.moon@gmail.com>
2024-12-31 14:45:45 +0500

    add 20 function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = [];
+    let keyIndex = 0;
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (/[A-Z]/.test(message[i])) {
+        let charCodeMessage = message.charCodeAt(i);
+        let charCodeKey = key.charCodeAt(keyIndex % key.length);
+        let encryptedCharCode = (charCodeMessage - 65 + (charCodeKey - 65)) % 26 + 65;
+        result.push(String.fromCharCode(encryptedCharCode));
+
+        keyIndex++;
+      } else {
+        result.push(message[i]);
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = [];
+    let keyIndex = 0;
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (/[A-Z]/.test(message[i])) {
+        let charCodeMessage = message.charCodeAt(i);
+        let charCodeKey = key.charCodeAt(keyIndex % key.length);
+        let decryptedCharCode = (charCodeMessage - 65 - (charCodeKey - 65) + 26) % 26 + 65;
+        result.push(String.fromCharCode(decryptedCharCode));
+
+        keyIndex++;
+      } else {
+        result.push(message[i]);
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
   }
 }
 
